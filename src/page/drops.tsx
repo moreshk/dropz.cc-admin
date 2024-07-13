0dd1bdf856af0d2fa18e14d6b4add5270f3513fd
Pranav <pranavkp.me@outlook.com>
2024-07-13 10:20:36 +0530

    added tokens lists for the drops

--- src/page/drops.tsx
+++ src/page/drops.tsx
@@ -3,9 +3,11 @@ import useSWR from "swr";
 
 export const Drops = () => {
   const { data, isLoading } = useSWR<{ drops: Drop[] }>("/drop/all");
+  const { data: token, isLoading: isTokenLoading } = useSWR<{
+    tokens: TokenDetails[];
+  }>("/token");
 
-  console.log(data);
-  if (isLoading && !data) {
+  if ((isLoading && !data) || (!token && isTokenLoading)) {
     return <div>Loading...</div>;
   }
 
@@ -13,7 +15,7 @@ export const Drops = () => {
     <div className="p-5">
       <div className="flex justify-between items-center">
         <p>Drops(lottery)</p>
-        <DropsModal tokens={[]} />
+        <DropsModal tokens={token?.tokens || []} />
       </div>
       {data?.drops.map((drops: Drop) => (
         <div key={drops.id}>{drops.id}</div>
